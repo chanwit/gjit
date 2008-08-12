3e732f7a89bfcd8a332a91435ba0b857339706dd
U-LB\chanwit <chanwit@gmail.com>
2008-08-12 15:54:05 +0700

    add 2 testcases

---
+++ test/minimal/PreprocessSubject.groovy
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package minimal
+
+
+
+/**
+ * @author chanwit
+ *
+ */
+public class PreprocessSubject{
+	
+	def mymethod() {
+		
+	}
+
+	static main(arg) {
+		println "test"
+	}
+	
+}
---
+++ test/minimal/PreprocessTests.java
@@ -0,0 +1,25 @@
+package minimal;
+
+import java.io.RandomAccessFile;
+
+import org.codehaus.groovy.gjit.PreProcess;
+
+import junit.framework.TestCase;
+
+public class PreprocessTests extends TestCase {
+
+	public void testCheckGroovyFile() {
+		try {
+			RandomAccessFile f = new RandomAccessFile("test/minimal/PreprocessSubject.class","r");
+			byte[] bytes = new byte[(int) f.length()];
+			f.readFully(bytes);
+			PreProcess p = PreProcess.perform(bytes);
+			assertEquals(true, p.isGroovyClassFile());
+			assertEquals(true, p.getMethods().containsKey("<init>()V"));
+			assertEquals(3, p.getMethods().size());
+		} catch(Throwable e) {
+			fail(e.getMessage());
+		}
+	}
+			
+}
