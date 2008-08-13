24f73cc9a010424487857a863d7d99b76af7e059
U-LB\chanwit <chanwit@gmail.com>
2008-08-14 05:12:47 +0700

    fixed regression due to refactoring

---
+++ test/minimal/intadd/Breakdown001Subject.groovy
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package minimal.intadd
+
+/**
+ * @author chanwit
+ *
+ */
+public class Breakdown001Subject{
+	
+	int add_006() {
+		int i = 0;
+		(5+5).times {
+			i=i+1;
+		}
+		return i;
+	}
+	
+}
