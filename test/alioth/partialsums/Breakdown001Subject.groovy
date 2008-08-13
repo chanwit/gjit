efa9ece9e5ded2b1b9ff212f8bef123959edd7c6
U-LB\chanwit <chanwit@gmail.com>
2008-08-13 23:51:45 +0700

    fixed 2 breakdown of partial sums

---
+++ test/alioth/partialsums/Breakdown001Subject.groovy
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package alioth.partialsums
+
+
+
+/**
+ * @author chanwit
+ *
+ */
+public class Breakdown001Subject{
+
+    double a1
+    double a2
+    double a3
+    double a4
+    double a5
+    double a6
+    double a7
+    double a8
+    double a9
+
+    def run(n) {
+        a1 = a2 = a3 = a4 = a5 = a6 = a7 = a8 = a9 = 0.0D
+    }
+	
+}
