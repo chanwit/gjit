f48e3d1e0b87364211d1682e4107652bbb4ddd68
U-LB\chanwit <chanwit@gmail.com>
2008-08-15 00:35:16 +0700

    starting to refactor fix dup

--- test/alioth/PartialSums.groovy
+++ test/alioth/PartialSums.groovy
@@ -5,7 +5,7 @@ package alioth
  *
  */
 public class PartialSums{
-    
+
     double a1
     double a2
     double a3
@@ -16,32 +16,32 @@ public class PartialSums{
     double a8
     double a9
 
-    def run(n) {
-        a1 = a2 = a3 = a4 = a5 = a6 = a7 = a8 = a9 = 0.0D
-        double twothirds = 2.0D/3.0D
-        
-        double alt = -1.0D
-        double k = 1.0D
+    def run(int n) {
+      a1 = a2 = a3 = a4 = a5 = a6 = a7 = a8 = a9 = 0.0D
+      double twothirds = 2.0D/3.0D
 
-        while (k <= n) {
-           double k2 = k * k
-           double k3 = k2 * k
-           double sk = Math.sin(k)
-           double ck = Math.cos(k)
-           alt = -alt
+      double alt = -1.0D
+      double k = 1.0D
 
-           a1 += twothirds**(k-1.0D)
-           a2 += 1.0D/Math.sqrt(k)
-           a3 += 1.0D/(k*(k+1.0D))
-           a4 += 1.0D/(k3*sk*sk)
-           a5 += 1.0D/(k3*ck*ck)
-           a6 += 1.0D/k
-           a7 += 1.0D/k2
-           a8 += alt/k
-           a9 += alt/(2.0D*k - 1.0D)
+      while (k <= n) {
+         double k2 = k * k
+         double k3 = k2 * k
+         double sk = Math.sin(k)
+         double ck = Math.cos(k)
+         alt = -alt
 
-           k += 1.0D
-        }
+         a1 += twothirds**(k-1.0D)
+         a2 += 1.0D/Math.sqrt(k)
+         a3 += 1.0D/(k*(k+1.0D))
+         a4 += 1.0D/(k3*sk*sk)
+         a5 += 1.0D/(k3*ck*ck)
+         a6 += 1.0D/k
+         a7 += 1.0D/k2
+         a8 += alt/k
+         a9 += alt/(2.0D*k - 1.0D)
+
+         k += 1.0D
+      }
     }
 
 //		printf("%.9f\t(2/3)^k\n", a1)
@@ -54,9 +54,9 @@ public class PartialSums{
 //		printf("%.9f\tAlternating Harmonic\n", a8)
 //		printf("%.9f\tGregory\n", a9)
 
-	static main(args) {
-		def n = Integer.parseInt(args[0])
-		new PartialSums().run(n)
-	}
-	
+  static main(args) {
+    def n = Integer.parseInt(args[0])
+    new PartialSums().run(n)
+  }
+
 }
