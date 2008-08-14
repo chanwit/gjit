50ab5ddd830bbaf573084d9d4f8f0ab62f22066d
U-LB\chanwit <chanwit@gmail.com>
2008-08-15 02:03:51 +0700

    starting no. 7

--- test/alioth/partialsums/Breakdown006Subject.groovy
+++ test/alioth/partialsums/Breakdown006Subject.groovy
@@ -29,20 +29,20 @@ public class Breakdown006Subject{
         double k = 1.0D
 
         while (k <= n) {
-//            double k2 = k * k
-//            double k3 = k2 * k
-//            double sk = Math.sin(k)
-//            double ck = Math.cos(k)
-//            alt = -alt
+            double k2 = k * k
+            double k3 = k2 * k
+            double sk = Math.sin(k)
+            double ck = Math.cos(k)
+            alt = -alt
             a1 += twothirds**(k-1.0D)
 //            a2 += 1.0D/Math.sqrt(k)
 //            a3 += 1.0D/(k*(k+1.0D))
 //            a4 += 1.0D/(k3*sk*sk)
 //            a5 += 1.0D/(k3*ck*ck)
-//            a6 += 1.0D/k
-//            a7 += 1.0D/k2
-//            a8 += alt/k
-//            a9 += alt/(2.0D*k - 1.0D)
+            a6 += 1.0D/k
+            a7 += 1.0D/k2
+            a8 += alt/k
+            a9 += alt/(2.0D*k - 1.0D)
             k += 1.0D
          }
     }
