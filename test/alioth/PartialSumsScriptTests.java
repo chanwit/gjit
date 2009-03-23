fbf37afdd1e7da6eabd7cbed54131f13c3409202
U-LB\chanwit <chanwit@gmail.com>
2009-03-23 09:22:48 +0700

    add script test, still failed

---
+++ test/alioth/PartialSumsScriptTests.java
@@ -0,0 +1,39 @@
+package alioth;
+
+import helper.Utils;
+
+import java.io.RandomAccessFile;
+import java.lang.reflect.Method;
+
+import org.codehaus.groovy.gjit.Optimiser;
+import org.codehaus.groovy.gjit.PreProcess;
+
+import junit.framework.TestCase;
+
+public class PartialSumsScriptTests extends TestCase {
+
+    private static Class<?> c;
+    private static byte[] out;
+    private static byte[] bytes;
+
+    static {
+        try {
+            RandomAccessFile f = new RandomAccessFile("test/partial_sums.class","r");
+            bytes = new byte[(int) f.length()];
+            f.readFully(bytes);
+            PreProcess p = PreProcess.perform(bytes);
+            out = Optimiser.perform(p, bytes);
+            Utils.dumpForCompare(bytes, out);
+            // c = Utils.loadClass("alioth.PartialSums", out);
+        } catch(Throwable e) {
+        }
+    }
+
+    public void testPartialSums() throws Throwable {
+        Method m = c.getMethod("run", int.class);
+        Object o = c.newInstance();
+        Object result = m.invoke(o, 10);
+        assertEquals(null, result);
+    }
+
+}
